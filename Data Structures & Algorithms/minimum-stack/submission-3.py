da31bfb997c17ec2a67836d2e1098799ed6d967e
sohanR18 <95858270+sohanR18@users.noreply.github.com>
2026-09-16 12:32:08 -0400

    Bulk sync: 194 submissions

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.py
@@ -0,0 +1,22 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.Minstack = []
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.Minstack[-1] if self.Minstack else val)
+        self.Minstack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.Minstack.pop()
+        
+
+    def top(self) -> int:
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return self.Minstack[-1]
+        
---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        ClosetoOpen = {")" : "(", "}":"{", "]":"["} 
+
+        for c in s:
+            if c in ClosetoOpen:
+                if stack and stack[-1] == ClosetoOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return True if not stack else False
